3b96e62f09b41a1a49b5ecb514536a1e3651d15b
youngvvp <andreyrussia111@gmail.com>
2022-04-27 01:48:21 +0300

    corrections hw2

--- src/p2-homeworks/h2/Affair.tsx
+++ src/p2-homeworks/h2/Affair.tsx
@@ -14,7 +14,7 @@ function Affair(props: AffairPropsType) {
     return (
         <div className={s.affair}>
             <div className={s.name}>{props.affair.name}</div>
-            <div className={s.priority}>{props.affair.priority}</div>
+            <div className={s[props.affair.priority]}>{props.affair.priority}</div>
 
             <button onClick={deleteCallback}>X</button>
         </div>
